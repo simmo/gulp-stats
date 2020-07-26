43db1894e88dd733447d68599e0c481b63795c81
Mike Simmonds <mike@simmo.me>
2020-07-26 18:38:05 +0100

    Hiding the table tweaking messaging for no tasks returned

--- src/reporters/log.test.ts
+++ src/reporters/log.test.ts
@@ -1,8 +1,13 @@
 import log from './log';
+import stripAnsi from 'strip-ansi';
 
 const logSpy = jest.spyOn(console, 'log').mockImplementation(data => data);
 
 describe('Log reporter', () => {
+	beforeEach(() => {
+		logSpy.mockClear();
+	});
+
 	test('formats and logs the report', () => {
 		log({
 			tasks: [
@@ -44,6 +49,61 @@ taskD  [36m400 ms[39m         [35m44%[39m
 taskA  [36m300 ms[39m         [35m33%[39m
 taskC  [36m200 ms[39m         [35m22%[39m
 taskB  [36m100 ms[39m         [35m11%[39m
+`);
+	});
+
+	test('use singular when 1 task', () => {
+		log({
+			tasks: [
+				{
+					name: 'taskD',
+					duration: 0.4,
+					durationHr: [0, 400000000],
+					durationPretty: '400 ms',
+				},
+			],
+			totalTime: 0.4,
+			totalTimeHr: [0, 400000000],
+			totalTimePretty: '400 ms',
+		});
+
+		expect(stripAnsi(logSpy.mock.results[0].value)).toContain('ran 1 task');
+	});
+
+	test('uses plural when 2 or more tasks', () => {
+		log({
+			tasks: [
+				{
+					name: 'taskD',
+					duration: 0.4,
+					durationHr: [0, 400000000],
+					durationPretty: '400 ms',
+				},
+				{
+					name: 'taskA',
+					duration: 0.3,
+					durationHr: [0, 300000000],
+					durationPretty: '300 ms',
+				},
+			],
+			totalTime: 0.7,
+			totalTimeHr: [0, 700000000],
+			totalTimePretty: '700 ms',
+		});
+
+		expect(stripAnsi(logSpy.mock.results[0].value)).toContain('ran 2 tasks ');
+	});
+
+	test('returns different messaging when no tasks provided', () => {
+		log({
+			tasks: [],
+			totalTime: 0,
+			totalTimeHr: [0, 0],
+			totalTimePretty: '0 ms',
+		});
+
+		expect(logSpy.mock.results[0].value).toBe(`
+Gulp ran [36m[1mno tasks[22m[39m in [36m[1m0 ms[22m[39m
 `);
 	});
 });
--- src/reporters/log.ts
+++ src/reporters/log.ts
@@ -3,28 +3,36 @@ import { Reporter } from '../utils/types';
 import table from 'text-table';
 import stripAnsi from 'strip-ansi';
 
+const header = (what: string, time: string) =>
+	`\nGulp ran ${chalk.cyan.bold(what)} in ${chalk.cyan.bold(time)}\n`;
+
 const logReporter: Reporter = ({ tasks, totalTime, totalTimePretty }) => {
-	const formattedTasks = table(
-		[
-			['Task', 'Time', '% of total'].map(header =>
-				chalk.bold.underline(header)
-			),
-			...tasks.map(({ duration, durationPretty, name }) => [
-				name,
-				chalk.cyan(durationPretty),
-				chalk.magenta(Math.round((duration / totalTime) * 100) + '%'),
-			]),
-		],
-		{
-			align: ['l', 'r', 'r'],
-			stringLength: (string: string) => stripAnsi(string).length,
-		}
-	);
+	if (tasks.length === 0) {
+		console.log(header('no tasks', totalTimePretty));
+
+		return;
+	}
 
 	console.log(
-		`\nGulp ran ${chalk.cyan.bold(
-			`${tasks.length} task${tasks.length === 1 ? '' : 's'}`
-		)} in ${chalk.cyan.bold(totalTimePretty)}\n\n${formattedTasks}\n`
+		`${header(
+			`${tasks.length} task${tasks.length === 1 ? '' : 's'}`,
+			totalTimePretty
+		)}\n${table(
+			[
+				['Task', 'Time', '% of total'].map(header =>
+					chalk.bold.underline(header)
+				),
+				...tasks.map(({ duration, durationPretty, name }) => [
+					name,
+					chalk.cyan(durationPretty),
+					chalk.magenta(Math.round((duration / totalTime) * 100) + '%'),
+				]),
+			],
+			{
+				align: ['l', 'r', 'r'],
+				stringLength: (string: string) => stripAnsi(string).length,
+			}
+		)}\n`
 	);
 };
 
